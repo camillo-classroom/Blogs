3dd7810401da91b6e924e07d4de6da4a8d5fe62a
Camillo <camillo@principalx.com.br>
2025-06-06 16:58:13 -0300

    Chave privada para assinatura do token

---
+++ Blogs.Api/Config.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace Blogs.Api;
+
+public class Config
+{
+    static Config()
+    {
+        var caminho = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.secrets.json");
+        if (File.Exists(caminho))
+            instancia = JsonSerializer.Deserialize<Config>(File.ReadAllText(caminho));
+        else
+            throw new Exception("Configuração da chave privada inexistente");
+    }
+
+    public string ChavePrivada { get; set; } = "";
+    public static Config Instancia => instancia ?? new Config();
+    private static readonly Config? instancia = null;
+}
