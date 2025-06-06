8206a0b5da45072de1908ac61156f4b48f1cc525
Camillo <camillo@principalx.com.br>
2025-06-06 16:59:16 -0300

    Controle de tokens

---
+++ Blogs.Api/ClaimExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace Blogs.Api
+{
+    public static class ClaimExtensions
+    {
+            public static long GetId(this ClaimsPrincipal user)
+            {
+                try
+                {
+                    return Convert.ToInt64(user.Claims.FirstOrDefault(c => c.Type == "Id")?.Value ?? "0");
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+}
---
+++ Blogs.Api/TokenService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using Blogs.DTO.ControleAcessos;
+
+namespace Blogs.Api;
+
+public class TokenService
+{
+    public string Gerar(UsuarioDTO usuario)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.CreateToken(GetTokenDescriptor(usuario));
+        return handler.WriteToken(token);
+    }
+
+    private static SecurityTokenDescriptor GetTokenDescriptor(UsuarioDTO usuario)
+    {
+        return new SecurityTokenDescriptor
+        {
+            Subject = GerarClaims(usuario),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = GetCredentials(),
+        };
+    }
+
+    private static SigningCredentials GetCredentials()
+    {
+        var key = Encoding.ASCII.GetBytes(Config.Instancia.ChavePrivada);
+
+        return new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256Signature
+        );
+    }
+
+    private static ClaimsIdentity GerarClaims(UsuarioDTO usuario)
+    {
+        var ci = new ClaimsIdentity();
+
+        ci.AddClaim(new Claim(ClaimTypes.Name, usuario.Email));
+        ci.AddClaim(new Claim("Id", usuario.Id.ToString()));
+
+        //foreach (var role in usuario.Roles)
+        //    ci.AddClaim(new Claim(ClaimTypes.Role, role));
+
+        return ci;
+    }
+}
